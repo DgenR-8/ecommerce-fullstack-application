5927de866eb17c1b200516996b8a4e022edbbcec
DgenR-8 <155558267+DgenR-8@users.noreply.github.com>
2026-01-16 04:59:44 +0000

    Ecommerce source files
    Base files for development

---
+++ src/main/java/com/devdt/DT/Ecommerce/DtEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.devdt.DT.Ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DtEcommerceApplication.class, args);
+	}
+
+}
