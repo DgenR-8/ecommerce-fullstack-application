bf69dc2bdde3213c25a723c34ad8dbf7bb1c4b60
DgenR-8 <155558267+DgenR-8@users.noreply.github.com>
2026-01-16 05:57:20 +0000

    Exceptions added

---
+++ src/main/java/com/devdt/DT/Ecommerce/exception/InvalidCredentialsException.java
@@ -0,0 +1,8 @@
+package com.devdt.DT.Ecommerce.exception;
+
+public class InvalidCredentialsException extends RuntimeException{
+
+    public InvalidCredentialsException(String message) {
+        super(message);
+    }
+}
