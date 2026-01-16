dbe65b978419ef961373d0cefec92c496880239a
DgenR-8 <155558267+DgenR-8@users.noreply.github.com>
2026-01-16 06:06:52 +0000

    Security - CustomUserDetailsService added

---
+++ src/main/java/com/devdt/DT/Ecommerce/security/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.devdt.DT.Ecommerce.security;
+
+import com.devdt.DT.Ecommerce.entity.User;
+import com.devdt.DT.Ecommerce.exception.NotFoundException;
+import com.devdt.DT.Ecommerce.repository.UserRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepo userRepo;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepo.findByEmail(username)
+                .orElseThrow(()-> new NotFoundException("User/ Email not found"));
+
+        return AuthUser.builder()
+                .user(user)
+                .build();
+    }
+}
