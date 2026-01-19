cc6b28408d2d8d6af9883e4ec32dd05e938c3fd2
DgenR-8 <155558267+DgenR-8@users.noreply.github.com>
2026-01-19 21:33:06 +0000

    Enums added

---
+++ src/main/java/com/devdt/DT/Ecommerce/enums/OrderStatus.java
@@ -0,0 +1,5 @@
+package com.devdt.DT.Ecommerce.enums;
+
+public enum OrderStatus {
+    PENDING, CONFIRMED, SHIPPED, DELIVERED, CANCELLED, RETURNED
+}
---
+++ src/main/java/com/devdt/DT/Ecommerce/enums/UserRole.java
@@ -0,0 +1,5 @@
+package com.devdt.DT.Ecommerce.enums;
+
+public enum UserRole {
+    ADMIN, USER
+}
